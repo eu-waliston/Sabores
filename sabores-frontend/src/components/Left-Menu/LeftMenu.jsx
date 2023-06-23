24fdc0c1f89ef528a640458b4372ca937bc5fe90
Waliston <goneisgone@hotmail.com>
2023-06-23 09:45:06 -0300

    Sabores Website - Lateral Menu 90%

--- sabores-frontend/src/components/Left-Menu/LeftMenu.jsx
+++ sabores-frontend/src/components/Left-Menu/LeftMenu.jsx
@@ -11,15 +11,14 @@ const LeftMenu = () => {
 
   return (
     <div className={nav ? "LeftMenu-Component" : "LeftMenu-Component-clicked"}>
-      <i
-        class="fa-solid fa-bars-staggered btn-effect"
-        onClick={handleClick}
-      ></i>
-      {/* <i class="fa-solid fa-xmark"></i> */}
+      <i class={nav ? "btn-effect fa-solid fa-circle-chevron-left" :  "btn-effect fa-solid fa-circle-chevron-right"  } onClick={handleClick}></i>
+
       <div className="leftmenu-itens">
         <div className="leftmenu--iten">
           <i class="fa-solid fa-cake-candles"></i>
-          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>Bolos e Tortas</h1>
+          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>
+            Bolos e Tortas
+          </h1>
         </div>
 
         <div className="leftmenu--iten">
@@ -34,12 +33,16 @@ const LeftMenu = () => {
 
         <div className="leftmenu--iten">
           <i class="fa-solid fa-fish"></i>
-          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>Peixes e Frutos do Mar</h1>
+          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>
+            Peixes e Frutos do Mar
+          </h1>
         </div>
 
         <div className="leftmenu--iten">
-          <i class="fa-solid fa-utensils"></i>
-          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>Saladas e Molhos</h1>
+        <i class="fa-solid fa-bowl-rice"></i>
+          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>
+            Saladas e Molhos
+          </h1>
         </div>
 
         <div className="leftmenu--iten">
@@ -59,7 +62,9 @@ const LeftMenu = () => {
 
         <div className="leftmenu--iten">
           <i class="fa-solid fa-cookie-bite"></i>
-          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>Doces e Sobremesas</h1>
+          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>
+            Doces e Sobremesas
+          </h1>
         </div>
 
         <div className="leftmenu--iten">
@@ -68,13 +73,17 @@ const LeftMenu = () => {
         </div>
 
         <div className="leftmenu--iten">
-          <i class="fa-solid fa-heart"></i>
-          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>Alimentação Saudável</h1>
+        <i class="fa-solid fa-heart-pulse"></i>
+          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>
+            Alimentação Saudável
+          </h1>
         </div>
 
         <div className="leftmenu--iten">
           <i class="fa-solid fa-kitchen-set"></i>
-          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>Todas as Receitas</h1>
+          <h1 className={nav ? "leftmenu--iten-h1" : "h1-effect"}>
+            Todas as Receitas
+          </h1>
         </div>
       </div>
     </div>
