7ccafb13a2d31dd2b17f888443517df43c9ffa27
Waliston <goneisgone@hotmail.com>
2023-08-18 17:07:22 -0300

    Sabores, backend hash password

--- sabores-backend/src/Controller/user.controller.js
+++ sabores-backend/src/Controller/user.controller.js
@@ -1,4 +1,7 @@
 const User = require("../Model/user");
+require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 async function getAllUsers(req, res) {
   try {
@@ -19,20 +22,62 @@ async function getAnUser(req, res) {
 }
 
 async function createAuser(req, res) {
-  const newUser = new User({
-    username: req.body.username,
-    password: req.body.password,
-    email: req.body.email,
-    dateNasc: req.body.dateNasc,
-    role: req.body.role,
-  });
+  // const newUser = new User({
+  //   username: req.body.username,
+  //   password: req.body.password,
+  //   email: req.body.email,
+  //   dateNasc: req.body.dateNasc,
+  //   role: req.body.role,
+  // });
 
-  try {
-    const saveUser = await newUser.save();
-    res.json(saveUser);
-  } catch (error) {
-    res.json({ message: error });
+  // try {
+  //   const saveUser = await newUser.save();
+  //   res.json(saveUser);
+  // } catch (error) {
+  //   res.json({ message: error });
+  // }
+
+  const { username, password, email, dateNasc, role } = req.body;
+
+  if (password.length < 0) {
+    return res.status(400).json({
+      message: "Password less than 6 characters",
+    });
   }
+
+  bcrypt.hash(password, 10).then(async (hash) => {
+    await User.create({
+      username,
+      password: hash,
+      email,
+      dateNasc,
+      role,
+    })
+      .then((user) => {
+        const maxAge = 3 * 60 * 60;
+        const token = jwt.sign(
+          { id: user._id, username, role: user.role },
+          process.env.JWT_SECRET,
+          {
+            expiresIn: maxAge,
+          }
+        );
+        res.cookie("jwt", token, {
+          httoOnly: true,
+          maxAge: maxAge * 1000,
+        });
+        res.status(200).json({
+          message: "User sucessfully created",
+          user,
+        });
+      })
+      .catch((error) => {
+        res.status(400).json({
+          message: "User not sucessfully created",
+          error: error.message,
+        });
+      });
+  });
 }
 
 async function updatedUser(req, res) {
